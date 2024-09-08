73483cc8747f7eb2c1cabbc0a93dc0f05b5b6ac6
Nikita <nikita@skyhive.io>
2024-09-08 17:49:02 +0300

    HomeWork5.1

---
+++ jsBasics/HomeWork5.1/task5.1.3.js
@@ -0,0 +1,13 @@
+const number = 9; 
+console.log("Multiplication table for " + number );
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + (number *+i));
+}
+
+
+console.log("Multiplication table for " + number );
+let a = 1;
+while (a <= 10) {
+    console.log(number + " x " + a + " = " + (number * a));
+    a++;
+}
