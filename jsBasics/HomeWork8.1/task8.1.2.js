f0e1b539126d5d921a6a80688ef350e4318d1e75
Nikita <01123niko@gmail.com>
2024-09-21 20:16:26 +0300

    Home Work 8.1

---
+++ jsBasics/HomeWork8.1/task8.1.2.js
@@ -0,0 +1,5 @@
+const mainArray = [1, 2, 3, 4, 5];
+
+const secondArray = mainArray.map((value, index) => value * index);
+
+console.log(secondArray);
---
+++ jsBasics/HomeWork8.1/task8.1.4.js
@@ -0,0 +1,5 @@
+const mainArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = mainArray.filter((mainArray) => mainArray % 2 === 0);
+
+console.log(evenNumbers);
